f5665518b13eee8a6f1006100f73ae79967081a3
juyalanshika0006 <juyalanshika6@email.com>
2026-06-22 11:18:36 +0530

    update

--- day01_pandas_drug_data.py
+++ day01_pandas_drug_data.py
@@ -6,12 +6,19 @@ df=pd.DataFrame({
     "Toxicity":[2,8,1,3,5],
     "Target":["EGFR","EGFR","TP53","TP53","EGFR"]
 })
-#print(df.shape,df.info(),df.head())
-#print(df.select_dtypes(include='number'))
-#print(df.select_dtypes(include='object'))
-#df["IC50"] = df["IC50"].fillna(25)#fill missing values
-#df["Hit"] = df["IC50"] < 20
-#hits=df[df["Hit"]]
-#print(df)
+# To identify the shape of the dataframe
+print(df.shape,df.info(),df.head())
+# To see which section includes numerical values
+print(df.select_dtypes(include='number'))
+# To see which section includes the text
+print(df.select_dtypes(include='object'))
+#fill missing values
+df["IC50"] = df["IC50"].fillna(25)
+#Creating a subset of Hit target molecule 
+df["Hit"] = df["IC50"] < 20
+hits=df[df["Hit"]]
+print(df)
+# Grouping all the Hit  target and obtaining their mean  
 print(df.groupby("Target")["IC50"].mean())
+#find Nitrogen- containing molecules
 print(df["SMILES"].str.contains("N"))
